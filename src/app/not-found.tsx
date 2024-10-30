b0b73f0eb2ff90fd0fe8776361fa2197ab397094
Ashford <ashfordrebelo147@gmail.com>
2024-10-30 12:37:13 +0530

    new

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import Image from 'next/image';
 import styles from './NotFound.module.css'; // Import your CSS module
 
-export default function NotFound(){
+export default function NotFound() {
     return (
         <div className={styles.container}>
-            <h2 className={styles.title}>Page not found</h2>
+            <h2 className={styles.title}>Page Not Found</h2>
             <p className={styles.message}>Oops! Looks like the page you're looking for doesn't exist.</p>
-            <img src="Images/notfound.gif" alt="Page not found" className={styles.gif} />
+            <Image 
+                src="/Images/notfound.gif" // Make sure this path is correct
+                alt="Page not found" 
+                className={styles.gif} 
+                width={500} // Set appropriate width
+                height={300} // Set appropriate height
+                priority // Load the image with high priority if it's crucial for the user experience
+            />
             <Link href="/Home">
                 <span className={styles.link}>Return Home</span>
             </Link>
